7df4cf5edde41ea010fd8521a6eca57f84256f86
Jason <jasontan2359@gmail.com>
2022-07-18 23:24:19 +0800

    Added back test 5, 11, 12

---
+++ UnitTest11.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+
+using System;
+
+namespace FYPUITest
+{
+    [TestClass]
+    public class UnitTest11
+    {
+        [TestMethod]
+        public void TestDeleteMovieAsMember()
+        {
+            string URL = "http://fypmovie.azurewebsites.net/Account/Login";
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl(URL);
+
+            By userIdBar = By.Name("UserID");
+            By passwordBar = By.Name("Password");
+
+            webDriver.FindElement(userIdBar).SendKeys("AwJie");
+            webDriver.FindElement(passwordBar).SendKeys("password2");
+            IWebElement loginButton = webDriver.FindElement(By.XPath("/html/body/div[1]/form/div/div[3]/input"));
+            loginButton.Click();
+
+            IWebElement movieButton = webDriver.FindElement(By.XPath("/html/body/nav/div/ul[1]/li[2]/a"));
+            movieButton.Click();
+
+            IWebElement deleteButton = webDriver.FindElement(By.XPath("/html/body/div[1]/div/table/tbody/tr[3]/td[8]/a"));
+            deleteButton.Click();
+
+            IAlert alert = webDriver.SwitchTo().Alert();
+            alert.Accept();
+
+
+            IWebElement actualResult = webDriver.FindElement(By.XPath("//h1[text()='Forbidden']"));
+
+            Assert.IsTrue(actualResult.Text.Equals("Forbidden"));
+
+            webDriver.Quit();
+        }
+    }
+}
